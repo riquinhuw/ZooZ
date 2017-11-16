f900ddafcf281f1fb77c1e7070d8d94a5ff75e2b
Carlos Ribeiro <riquinhuw_lass@hotmail.com>
2017-11-16 14:42:07 -0300

    Add project files.

---
+++ ZooZ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooZ
+{
+    /*YO povo lindo, bem como todos sabem sou fã de "comentarios" então vou encher o codigo com eles, MUAH MUAH MUAH   
+     * 
+     * Bem temos duas semanas para terminar tudo, começando hoje ( 16/11/2017)
+     * Vamos dar tudo de nos nesse JOB que vai ser sucesso, tão grande que iremos estagiar o Zoo de Dois Irmãos, gogog!
+     * 
+     * Depois Farei uma "Quadro" para por como anda o nosso desenvolvimento.
+     * 
+     * Para Duvidas de o que fazer é só entrar no Disord, lá terá todas as classes e metodos basicos necessarios
+     * 
+     * Metas extendidas: Fazer no Windows forms e com mapinha e tudo !!!! Até com sistema de cliente fidelidade, caixa e pá :)
+     * 
+     * MÃOS NA OBRÃ, pera quero dizer, DEDOS NO TECLADO!!! 
+     * 
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
